9dba1e1f0f1bf301f35f9a4ba010285ea7253b0a
n1mnull <3980039@gmail.com>
2015-10-30 10:48:32 +0200

    testGetFigureWhenFigureNotSet - added testGetFigureWhenXLessThenZero - added

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exception.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -30,5 +31,32 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure,actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenFigureNotSet() throws Exception {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenXLessThenZero() throws Exception {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+
+        try {
+
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointException e) {
+
+        }
+
     }
 }
