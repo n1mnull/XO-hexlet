c38ad9d4dc3e6d0ef680ad19d33ceca259d9f710
n1mnull <3980039@gmail.com>
2015-11-01 11:30:49 +0200

    Field  - refactoring, change check point on empty space

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
-    private static final int MIN_COORDINATE = 3;
+    private static final int MIN_COORDINATE = 0;
     private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
@@ -24,14 +24,13 @@ public class Field {
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
-                                                                        AlreadyOccupaiedException{
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException{
         if (!checkPoint(point)){
             throw new InvalidPointException();
         }
-        if (field[point.x][point.y] != null){
-            throw new AlreadyOccupaiedException();
-        }
+        //if (field[point.x][point.y] != null){
+        //    throw new AlreadyOccupaiedException();
+        //}
         field[point.x][point.y] = figure;
     }
 
@@ -41,7 +40,7 @@ public class Field {
     }
 
     private boolean checkCoordinate(final int coordinate){
-        return coordinate >=MIN_COORDINATE && coordinate<MAX_COORDINATE;
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 
 }
