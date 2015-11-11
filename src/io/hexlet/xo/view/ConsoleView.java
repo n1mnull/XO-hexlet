79f67a1e91db8d9c9a92ce8d06fcc3fd547d351e
Ivan <3980039@gmail.com>
2015-11-11 12:39:13 +0200

    ConsoleView  - added

---
+++ src/io/hexlet/xo/view/ConsoleView.java
@@ -0,0 +1,20 @@
+package io.hexlet.xo.view;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Game;
+
+public class ConsoleView {
+
+    public void show(final Game game){
+        final Field field = game.getField();
+
+        for (int x = 0; x < field.getSize();x++){
+
+        }
+
+    }
+
+    public void move(final Game game){
+
+    }
+}
